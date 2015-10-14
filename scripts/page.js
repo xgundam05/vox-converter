f3235328576d6c75a8cf6c3a04528f167df2252e
Matthew Anderson <anderson_matthew@live.com>
2015-10-14 15:28:42 -0400

    Update page.js

--- scripts/page.js
+++ scripts/page.js
@@ -9,6 +9,7 @@ var win = gui.Window.get();
 var isDOMLoaded = false;
 var jobs = [];
 var scene, camera, renderer, clock, controls;
+var worker;
 
 // Handle unloading of models and such
 var models = [];
@@ -28,7 +29,7 @@ window.onload = function(){
   Initialize();
 
   // Create the worker and process it's events
-  var worker = new WebWorker('scripts/geoGenerator.js');
+  worker = new Worker('scripts/geoGenerator.js');
   worker.addEventListener('message', function(e){
     var data = e.data;
     if (!data.hasOwnProperty('cmd'))
@@ -46,6 +47,8 @@ window.onload = function(){
       case 'status':
         break;
       default:
+        console.log('default');
+        console.log(data);
         break;
     }
   }, false);
@@ -55,14 +58,14 @@ window.onload = function(){
 
 function Update(){
   var frameTime = 0;
-  clock.GetDelta();
-  while (jobs.length > 0 && (time += clock.GetDelta()) < 0.016){
+  clock.getDelta();
+  while (jobs.length > 0 && (frameTime += clock.getDelta()) < 0.016){
     var data = jobs.shift();
 
     var geo = new THREE.Geometry();
     var vBuffer = new Float32Array(data.vertices);
     var fBuffer = new Int32Array(data.faces);
-    var cBuffer = new UInt8Array(data.colors);
+    var cBuffer = new Uint8Array(data.colors);
 
     for (var i = 0; i < vBuffer.length; i += 3){
       geo.vertices.push(
@@ -90,32 +93,37 @@ function Update(){
       geo.faces[index].vertexColors[0] = new THREE.Color(color);
 
       color = cBuffer[i + 1] == 0 ? lite : cBuffer[i + 1] == 1 ? mid : dark;
-      geo.faces[index].vertexColors[0] = new THREE.Color(color);
+      geo.faces[index].vertexColors[1] = new THREE.Color(color);
 
       color = cBuffer[i + 2] == 0 ? lite : cBuffer[i + 2] == 1 ? mid : dark;
-      geo.faces[index].vertexColors[0] = new THREE.Color(color);
+      geo.faces[index].vertexColors[2] = new THREE.Color(color);
     }
 
     // Compute normals
     geo.computeFaceNormals();
     geo.computeBoundingBox();
-    goe.computeBoundingSphere();
+    geo.computeBoundingSphere();
 
-    var mat = new THREE.MeshBasicMaterial({
-      vertexColors: THREE.VertexColors
+    var mat = new THREE.MeshLambertMaterial({
+      color: 0x5c79a9//vertexColors: THREE.VertexColors
     });
 
     var mesh = new THREE.Mesh(geo, mat);
 
-    if (models[job.id] === undefined)
-      models[job.id] = [];
-    models[job.id].push(mesh);
+    if (models[data.id] === undefined)
+      models[data.id] = [];
+    models[data.id].push(mesh);
 
     scene.add(mesh);
+    Render();
   }
 
   controls.update();
   requestAnimationFrame(Update);
+}
+
+function Render(){
+  renderer.render(scene, camera);
 }
 
 function Initialize(){
@@ -136,11 +144,18 @@ function Initialize(){
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   // Controls
-  controls = new THREE.OrbitControls(camera);
+  controls = new THREE.OrbitControls(camera, document.getElementById('canv'));
   controls.addEventListener('change', function(){
-    renderer.render(scene, camera);
+    Render();
   });
-  controls.target = new THREE.Vector3(0, 0, 0);
+  controls.target.set(0, 0, 0);
+
+  // lights
+  var dLight = new THREE.DirectionalLight(0xffffff, 0.7);
+  var aLight = new THREE.AmbientLight(0xa0a0a0);
+  dLight.position.set(3, 4, 5);
+  scene.add(dLight);
+  scene.add(aLight);
 
   // Renderer
   renderer = new THREE.WebGLRenderer({
@@ -166,11 +181,71 @@ function Initialize(){
 }
 
 function handleFileOpen(e){
-  // TODO
+  var filePath = this.value;
+  var mdl = magicaVoxel.load(filePath);
+
+  var buffer = new ArrayBuffer(mdl.width * mdl.height * mdl.depth);
+  var view = new Uint8Array(buffer);
+
+  if (mdl.zUp){
+    for (var z = 0; z < mdl.depth; z++){
+      for (var y = 0; y < mdl.height; y++){
+        for (var x = 0; x < mdl.width; x++){
+          var tmp = mdl.data[x + (y * mdl.width) + (z * mdl.width * mdl.height)];
+          var index = x + (z * mdl.width) + ((mdl.height - 1 - y) * mdl.width * mdl.depth);
+          view[index] = tmp;
+        }
+      }
+    }
+  }
+  else {
+    for (var i = 0; i < mdl.data.length; i++){
+      view[i] = mdl.data[i];
+    }
+  }
+
+  var id = models.length;
+  worker.postMessage({
+    cmd: 'create',
+    mdl: {
+      id: id,
+      width: mdl.width,
+      height: mdl.zUp ? mdl.depth : mdl.height,
+      depth: mdl.zUp ? mdl.height : mdl.depth,
+    },
+    buffer: buffer
+  }, [buffer]);
+
+  camera.position.set(
+    mdl.width / 2,
+    mdl.zUp ? mdl.depth * 2 : mdl.height * 2,
+    mdl.zUp ? mdl.height * 2 : mdl.depth * 2
+  );
+
+  controls.target.set(
+    mdl.width / 2,
+    mdl.zUp ? mdl.depth / 2 : mdl.height / 2,
+    mdl.zUp ? mdl.height / 2 : mdl.depth / 2
+  );
+
+  ClearModelsTo(id);
 }
 
 function handleFileSave(e){
   // TODO
+}
+
+function ClearModelsTo(index){
+  for (var i = 0; i < index; i++){
+    var chunks = models[i];
+    if (chunks !== undefined)
+    for (var j = 0; j < chunks.length; j++){
+      scene.remove(chunks[j]);
+      chunks[j] = undefined;
+    }
+
+    models[i] = undefined;
+  }
 }
 
 // Create the Window Menu
